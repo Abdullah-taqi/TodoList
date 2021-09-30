c360e454abf393cd377eaa8e53c4f3ad62af47d3
Abdullah Taqi <ataqi99@gmail.com>
2021-09-30 15:06:30 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,89 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+//event listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
+
+
+
+
+
+//functions
+function addTodo(event){
+    event.preventDefault();
+    // todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    // check mark button
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML='<i class="fa fa-check"></i>';
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+    // check trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML='<i class="fa fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+    //clear todo input value
+    todoInput.value=" ";
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    //delete todo
+    if(item.classList[0]=='trash-btn'){
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
+    }
+    // check mark
+    if(item.classList[0]=='complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+        item.classList.toggle('success');
+        
+       
+    }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+        case "all":
+            todo.style.display = "flex";
+            break;
+        
+        case "completed":
+            if(todo.classList.contains("completed")){
+                todo.style.display = "flex";
+            }else{
+                todo.style.display = "none";
+            }
+            break;
+        case "incomplete":
+            if(!todo.classList.contains("completed")){
+                todo.style.display = "flex";
+            }else{
+                todo.style.display = "none";
+            }
+            break;
+    }
+        
+    });
+}
